efee632e88262db7e525edfd51581d2c2620b383
Filagy <filagentleman@mail.ru>
2020-11-27 13:12:21 +0300

    Вывели две точки на экран

--- TetrisNew/TetrisNew/Program.cs
+++ TetrisNew/TetrisNew/Program.cs
@@ -6,7 +6,28 @@ namespace TetrisNew
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            int x1 = 2;
+            int y1 = 3;
+            char z1 = '*';
+
+            Drow(x1, y1, z1);
+
+            int x2 = 4;
+            int y2 = 6;
+            char z2 = '#';
+
+            Drow(x2, y2, z2);
+
+            Console.ReadKey();
+
+        }
+        static void Drow(int x,int y, char z)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(z);
         }
     }
 }
