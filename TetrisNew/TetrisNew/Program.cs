9ffb5bfd7407344e49b18ee4b8d19c4ab6ffc784
Filagy <filagentleman@mail.ru>
2020-11-27 13:17:50 +0300

    Drow - Draw

--- TetrisNew/TetrisNew/Program.cs
+++ TetrisNew/TetrisNew/Program.cs
@@ -13,18 +13,18 @@ namespace TetrisNew
             int y1 = 3;
             char z1 = '*';
 
-            Drow(x1, y1, z1);
+            Draw(x1, y1, z1);
 
             int x2 = 4;
             int y2 = 6;
             char z2 = '#';
 
-            Drow(x2, y2, z2);
+            Draw(x2, y2, z2);
 
             Console.ReadKey();
 
         }
-        static void Drow(int x,int y, char z)
+        static void Draw(int x,int y, char z)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(z);
